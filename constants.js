bc419a8245b78a2d6863e68daef0ad9ae9d44cc4
kayama <yoichi.kayama@gmail.com>
2018-02-23 10:52:43 +0900

    debug

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 var constants = {
-  googleDocID: '2PACX-1vTaZ_tKbQiA6sQOhBwSxW6xeWpM42ER42otlA_w1pVkW1V_UB9zsi7-YjAAWCJYrIplLhTSwOssBth2'
+  googleDocID: '2PACX-1vTaZ_tKbQiA6sQOhBwSxW6xeWpM42ER42otlA_w1pVkW1V_UB9zsi7-YjAAWCJYrIplLhTSwOssBth2',
   informationSheetName: 'Information',
   pointsSheetName: 'Points'
 };
